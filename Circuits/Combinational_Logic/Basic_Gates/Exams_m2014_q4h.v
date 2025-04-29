3132074e1dc967de92b58f13ef48dcd72b2dda98
Dhanesh C N E <93080135+dhanesh15@users.noreply.github.com>
2025-04-29 16:24:01 +0530

    Create Exams_m2014_q4h.v

---
+++ Circuits/Combinational_Logic/Basic_Gates/Exams_m2014_q4h.v
@@ -0,0 +1,10 @@
+//Link : https://hdlbits.01xz.net/wiki/Exams/m2014_q4h
+//Explanation : Output should be equal to the input
+
+module top_module (
+    input in,
+    output out);
+    
+    assign out = in;
+
+endmodule

7db1a94d824c9b642f5b811897745d9dfe418e79
Dhanesh C N E <93080135+dhanesh15@users.noreply.github.com>
2025-04-29 16:04:58 +0530

    Create Zero.v

---
+++ Getting_Started/Zero.v
@@ -0,0 +1,8 @@
+//Link : https://hdlbits.01xz.net/wiki/Zero
+//Explanation: Output must be constant 0
+
+module top_module(
+    output zero
+);// Module body starts after semicolon
+	assign zero = 0;
+endmodule

3a9a4b202f7afcbed26a8c92464ea7c675bf0135
Dhanesh C N E <93080135+dhanesh15@users.noreply.github.com>
2025-04-29 17:50:18 +0530

    Create 01_Sim_circuit1.v

---
+++ Verification_Reading_Simulations/Build_Circuit_From_Simulation_Waveform/01_Sim_circuit1.v
@@ -0,0 +1,11 @@
+//Link : https://hdlbits.01xz.net/wiki/Sim/circuit1
+//Explanation : Output is 1 when both inputs are 1, this resembles AND Gate
+
+module top_module (
+    input a,
+    input b,
+    output q );//
+
+    assign q = a & b; // Fix me
+
+endmodule

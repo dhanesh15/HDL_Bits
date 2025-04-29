ecd7d787b6992c7435079a09182405e588cd916a
Dhanesh C N E <93080135+dhanesh15@users.noreply.github.com>
2025-04-29 16:25:28 +0530

    Create 02_Exams_m2014 q4i.v

---
+++ Circuits/Combinational_Logic/Basic_Gates/02_Exams_m2014 q4i.v
@@ -0,0 +1,7 @@
+//Link : https://hdlbits.01xz.net/wiki/Exams/m2014_q4i
+//Explanation : Output should be tied to GND (or 0)
+
+module top_module (
+    output out);
+    assign out = 1'b0;
+endmodule

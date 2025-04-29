caa990c9e36a407214b6bd1da5435701a2e40832
Dhanesh C N E <93080135+dhanesh15@users.noreply.github.com>
2025-04-29 16:12:52 +0530

    Create 04_Andgate.v

---
+++ Verilog_Language/01_Basics/04_Andgate.v
@@ -0,0 +1,10 @@
+//Link : https://hdlbits.01xz.net/wiki/Andgate
+//Explanation : Module should work like an AND Gate
+
+module top_module( 
+    input a, 
+    input b, 
+    output out );
+    assign out = a & b;
+
+endmodule

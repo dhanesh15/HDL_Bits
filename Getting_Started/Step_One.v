4d2f328a8f2492cf80410f34267c2ced8a6d790a
Dhanesh C N E <93080135+dhanesh15@users.noreply.github.com>
2025-04-29 16:02:29 +0530

    Create Step_One.v

---
+++ Getting_Started/Step_One.v
@@ -0,0 +1,9 @@
+//Link : https://hdlbits.01xz.net/wiki/Step_one
+//Explanation: The expectation is to drive 1 to the output
+
+module top_module( output one );
+
+// Insert your code here
+    assign one = 1;
+
+endmodule
